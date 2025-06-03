5bc1f1b79dee175089a506d6527c03dbc4f67050
EWGameOfDrones <ewgameofdrones@gmail.com>
2025-06-03 13:26:29 -0700

    split responsibiltity between cone beam rendering and cone ping handling

--- src/components/ConicalBeam.tsx
+++ src/components/ConicalBeam.tsx
@@ -1,18 +1,10 @@
 import {
-  Accessor,
   Component,
   createEffect,
   createMemo,
-  createSignal,
-  For,
-  getOwner,
-  on,
   onCleanup,
   onMount,
-  runWithOwner,
-  Show,
 } from 'solid-js'
-import { GridContext } from '../contexts/GridContext'
 import { useContextOrThrow } from '../util/useContextOrThrow'
 import {
   Color,
@@ -26,14 +18,12 @@ import { SensorContext } from '../contexts/SensorContext'
 import { metersPerFoot } from '../util/mathConstants'
 import { Brush, Evaluator, INTERSECTION } from 'three-bvh-csg'
 import { GraphingContext } from '../contexts/GraphingContext'
-import { createStore } from 'solid-js/store'
-import { ConicalBeamPing } from './ConicalBeamPing'
 import { formatHex, oklch } from 'culori'
+import { ConicalPingHandler } from './ConicalPingHandler'
 
 // display a conical beam using a sensor's properties
 export const ConicalBeam: Component = () => {
   const graphing = useContextOrThrow(GraphingContext)
-  const grid = useContextOrThrow(GridContext)
   const sensor = useContextOrThrow(SensorContext)
 
   // material to use for beam shapes
@@ -138,69 +128,9 @@ export const ConicalBeam: Component = () => {
     graphing.requestRender()
   })
 
-  // each ping has a memo for its distance. We use this component as the owner
-  // context for these memos
-  const owner = getOwner()
-
-  // track hieght in feet of last ping to display to use
-  const [getLastPingHeight, setLastPingHeight] = createSignal<Accessor<number>>(
-    () => 0
-  )
-
-  // the list of all actively displayed pings for the current sensor
-  const [pinsData, setPingsData] = createStore<{
-    pings: { distance: number }[]
-  }>({
-    pings: [],
-  })
-
-  sensor.data.setPingHandler(() => (centimeters: number) => {
-    runWithOwner(owner, () => {
-      const meters = centimeters / 100
-      const feet = meters / metersPerFoot
-
-      // save the height to be displayed to the user seperately
-      setLastPingHeight(() =>
-        createMemo(() => feet * Math.cos(sensor.calculate.phi()))
-      )
-      // create the ping to be displayed
-      setPingsData('pings', pinsData.pings.length, { distance: feet })
-    })
-  })
-
   return (
     <>
-      {/* create a ping object for each distance tracked */}
-      <For each={pinsData.pings}>
-        {(ping, getIndex) => (
-          <>
-            <ConicalBeamPing
-              distance={ping.distance}
-              coneBrush={getConeBrush()}
-              finish={() =>
-                setPingsData(
-                  'pings',
-                  pinsData.pings.filter((_, index) => index !== getIndex())
-                )
-              }
-            />
-          </>
-        )}
-      </For>
-      {/* if the height isn't 0, display the height to the user */}
-      <g class="fill-accent">
-        <text
-          x={`${grid.getXScale()(sensor.data.xFeet)}px`}
-          y={`${grid.getYScale()(sensor.data.yFeet) + 24}px`}
-          text-anchor="middle"
-        >
-          {Math.round(Math.abs(getLastPingHeight()() * 10)) / 10 !== 0
-            ? `${
-                getLastPingHeight()() > 0 ? '+' : ''
-              }${getLastPingHeight()().toFixed(1)}ft`
-            : ''}
-        </text>
-      </g>
+      <ConicalPingHandler coneBrush={getConeBrush()} />
     </>
   )
 }
