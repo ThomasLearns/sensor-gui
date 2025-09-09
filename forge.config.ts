0d36e13482230b9135fdff6ab8c25c76722d2021
William Black <william.s.black48.civ@us.navy.mil>
2025-09-09 08:45:15 -0700

    updated project make script to make for all available platforms

--- forge.config.ts
+++ forge.config.ts
@@ -3,6 +3,7 @@ import { MakerSquirrel } from '@electron-forge/maker-squirrel'
 import { VitePlugin } from '@electron-forge/plugin-vite'
 import { FusesPlugin } from '@electron-forge/plugin-fuses'
 import { FuseV1Options, FuseVersion } from '@electron/fuses'
+import { MakerZIP } from '@electron-forge/maker-zip'
 
 const config: ForgeConfig = {
   packagerConfig: {
@@ -15,8 +16,8 @@ const config: ForgeConfig = {
     onlyModules: ['serialport', 'drivelist'],
   },
   makers: [
-    new MakerSquirrel({}),
-    // new MakerZIP({}, ['darwin']),
+    // new MakerSquirrel({}),
+    new MakerZIP({}),
     // new MakerRpm({}),
     // new MakerDeb({}),
   ],
