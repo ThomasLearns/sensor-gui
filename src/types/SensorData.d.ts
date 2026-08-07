d9f5cbd85c0d678828efda5014f5bba77a993739
Thomas Luong (NAVAIR) <tcluong96@gmail.com>
2026-08-07 09:51:57 -0700

    Implement Drag-and-drop virtual missile launcer icon on GUI

--- src/types/SensorData.d.ts
+++ src/types/SensorData.d.ts
@@ -1,7 +1,7 @@
 import { Accessor, Component, Setter } from 'solid-js'
 
 interface SpecificSensor {
-  type: string
+  type: 'ultrasonic' | 'virtual_missile_launcher'
   renderer: Component<{}> // component used to render the sensor
 
   // already handled by "type" field, but other components of the sensor
@@ -43,9 +43,15 @@ export type UltrasonicData = SpecificSensor & {
   setResetMovementCalibration: Setter<undefined | (() => void)>
 }
 
+export type VirtualMissileLauncherData = SpecificSensor & {
+  type: 'virtual_missile_launcher'
+  measuringAngle: number
+  sensorTypeId: 2
+}
+
 // union of all sensor types
 // (only one currently, but more may be added in the future)
-export type SensorType = UltrasonicData
+export type SensorType = UltrasonicData | VirtualMissileLauncherData
 
 // all the information needed to display a sensor
 export type SensorData = {
